0314b1f0e219f39d62b90d307d2ae3a7d88fc1ca
eharpste <eharpste@cs.cmu.edu>
2016-07-18 12:13:29 -0400

    changed up how property 'M lines' get handled so that it should be more general, it now parses a whole 25000 sdf file from pubchem without error

--- convertmol.py
+++ convertmol.py
@@ -1,26 +1,27 @@
 import json
+from concept_formation.continuous_value import ContinuousValue
 from pprint import pprint
 
 radical_dict = {
-    0:"no radical",
+    0:"no_radical",
     1:"singlet",
     2:"doublet",
     3:"triplet"
 }
 
 charge_dict = {
-    0:"outside limits",
+    0:"outside_limits",
     1:"+3",
     2:"+2",
     3:"+1",
-    4:"doublet radical",
+    4:"doublet_radical",
     5:"-1",
     6:"-2",
     7:"-3"
 }
 
 stereo_parity_dict = {
-    0:"not stereo",
+    0:"not_stereo",
     1:"odd",
     2:"even",
     3:"unmarked"
@@ -40,38 +41,42 @@ bond_type_dict = {
     2:"Double",
     3:"Triple",
     4:"Aromatic",
-    5:"Single or Double",
-    6:"Single or Aromatic",
-    7:"Double or Aromatic",
+    5:"Single_or_Double",
+    6:"Single_or_Aromatic",
+    7:"Double_or_Aromatic",
     8:"Any"
 }
 
 single_bond_stereo_dict = {
-    0:"Not stereo",
+    0:"Not_stereo",
     1:"Up",
     4:"Either",
     6:"Down"
 }
 
 double_bond_stereo_dict = {
-    0:"Use coordinates",
-    3:"Cis or trans"
+    0:"Use_coordinates",
+    3:"Cis_or_trans"
 }
 
 def parse_counts_line(line):
-    #aaabbblllfffcccsssxxxrrrpppiiimmmvvvvvv
-    #aaa = number of atoms (current max 255)* 
-    #bbb = number of bonds (current max 255)* 
-    #lll = number of atom lists (max 30)* 
-    #fff = (obsolete)
-    #ccc = chiral flag: 0=not chiral, 1=chiral
-    #sss = number of stext entries 
-    #xxx = (obsolete)
-    #rrr = (obsolete)
-    #ppp = (obsolete)
-    #iii = (obsolete)
-    #mmm = number of lines of additional properties,
-    #vvvvv = version for the format
+    """
+    Parses the counts line of a molecule and returns it asd a dictionary
+
+    aaabbblllfffcccsssxxxrrrpppiiimmmvvvvvv
+    aaa = number of atoms (current max 255)* 
+    bbb = number of bonds (current max 255)* 
+    lll = number of atom lists (max 30)* 
+    fff = (obsolete)
+    ccc = chiral flag: 0=not chiral, 1=chiral
+    sss = number of stext entries 
+    xxx = (obsolete)
+    rrr = (obsolete)
+    ppp = (obsolete)
+    iii = (obsolete)
+    mmm = number of lines of additional properties,
+    vvvvv = version for the format
+    """
     ret = {}
     ret["aaa"] = int(float(line[0:3]))
     ret["bbb"] = int(float(line[3:6]))
@@ -83,23 +88,27 @@ def parse_counts_line(line):
     return ret
 
 def parse_atom_line(line):
-    #xxxxx.xxxxyyyyy.yyyyzzzzz.zzzz aaaddcccssshhhbbbvvvHHHrrriiimmmnnneee
-    #[0:10] xxxxx.xxxx = x-coordinate
-    #[10:20] yyyyy.yyyy = y-coordinate
-    #[20:30] zzzzz.zzzz = z-coordinate
-    #[31:34] aaa = atomic symbol
-    #[34:36] dd = mass difference, i.e. difference from standard mass
-    #[36:39] ccc = charge 0 = uncharged or value other than these, 1 = +3, 2 = +2, 3 = +1, 4 = doublet radical, 5 = -1, 6 = -2, 7 = -3
-    #[39:42] sss = atom stereo parity 0 = not stereo, 1 = odd, 2 = even, 3 = either or unmarked stereo center
-    #[42:45] hhh = INGORED hydrogen count +1
-    #[45:48] bbb = IGNORED stereo care box
-    #[48:51] vvv = valence
-    #[51:54] HHH = IGNORED H0 designator
-    #[54:57] rrr = Not used
-    #[57:60] iii = Not used
-    #[60:63] mmm = IGNORED atom-atom mapping number 1 - number of atoms
-    #[63:66] nnn = IGNORED inversion/retention flag g 0 = property not applied 1 = configuration is inverted,2 = configuration is retained
-    #[66:69] eee = IGNORED 0 = property not applied, 1 = change on atom must be exactly as shown
+    """
+    Parses a line from the atom block and returns it as a dictionary
+
+    xxxxx.xxxxyyyyy.yyyyzzzzz.zzzz aaaddcccssshhhbbbvvvHHHrrriiimmmnnneee
+    [0:10] xxxxx.xxxx = x-coordinate
+    [10:20] yyyyy.yyyy = y-coordinate
+    [20:30] zzzzz.zzzz = z-coordinate
+    [31:34] aaa = atomic symbol
+    [34:36] dd = mass difference, i.e. difference from standard mass
+    [36:39] ccc = charge 0 = uncharged or value other than these, 1 = +3, 2 = +2, 3 = +1, 4 = doublet radical, 5 = -1, 6 = -2, 7 = -3
+    [39:42] sss = atom stereo parity 0 = not stereo, 1 = odd, 2 = even, 3 = either or unmarked stereo center
+    [42:45] hhh = INGORED hydrogen count +1
+    [45:48] bbb = IGNORED stereo care box
+    [48:51] vvv = valence
+    [51:54] HHH = IGNORED H0 designator
+    [54:57] rrr = Not used
+    [57:60] iii = Not used
+    [60:63] mmm = IGNORED atom-atom mapping number 1 - number of atoms
+    [63:66] nnn = IGNORED inversion/retention flag g 0 = property not applied 1 = configuration is inverted,2 = configuration is retained
+    [66:69] eee = IGNORED 0 = property not applied, 1 = change on atom must be exactly as shown
+    """
     ret = {}
     ret["xxx"] = float(line[0:10])
     ret["yyy"] = float(line[10:20])
@@ -118,14 +127,18 @@ def parse_atom_line(line):
     return ret
 
 def parse_bond_line(line):
-    #111222tttsssxxxrrrccc
-    #111 = number of atom 1
-    #222 = number of atom 2
-    #ttt = bond type
-    #sss = bond stereo
-    #xxx = not used
-    #rrr = bond topology
-    #ccc = reacting center status
+    """
+    Parses a line from a bondblock and turns it into a dict
+
+    111222tttsssxxxrrrccc
+    111 = number of atom 1
+    222 = number of atom 2
+    ttt = bond type
+    sss = bond stereo
+    xxx = not used
+    rrr = bond topology
+    ccc = reacting center status
+    """
     ret = {}
     ret["111"] = int(float(line[0:3]))
     ret["222"] = int(float(line[3:6]))
@@ -136,60 +149,94 @@ def parse_bond_line(line):
     ret["ccc"] = int(float(line[18:21]))
     return ret
 
-def parse_prop_name(line):
+def parse_data_name(line):
+    """
+    Parses the name of a data item line, which will be used as an attribute name
+    """
     first = line.index("<")+1
     last = line.rindex(">")
     return line[first:last]
 
 def apply_m_chg(line,mol):
-    #print('apply_m_chg')
+    """
+    Parses a CHG line from the property block.
+
+    This will 0-out the charge on any atom that is not listed.
+    0123456789
+    M CHGnn8 aaa vvv ...
+    aaa An atom number to alter the charge for
+    vvv The ammount of charge on the target atom
+    """
+    if len(line) == 0:
+        return mol
     for k in mol:
         if k.startswith('?atom'):
-        #if k[0:5] == "?atom":
             mol[k]["charge"] = "0"
-    #print(line)
-    #print(line[9:])
-    for i in range(9,len(line),8):
-        aaa = line[i:i+4].strip()
-        vvv = line[i+4:i+8].strip()
-        #print(str(i),aaa,vvv)
-        #pprint(mol)
+    line = line.rsplit(r'\s+')[3:]
+    for i in range(0,len(line),2):
+        aaa = int(line[i])
+        vvv = line[i+1]
         if vvv[0] == "-":
-            mol["?atom"+aaa]["charge"] = vvv    
+            mol["?atom%04d"%aaa]["charge"] = vvv    
         else:
-            mol["?atom"+aaa]["charge"] = "+"+vvv
-#123456789A123456789B123456789
-#M  CHG  2   2  -1   5   1
+            mol["?atom%04d"%aaa]["charge"] = "+"+vvv
+    return mol
+
 def apply_m_rad(line,mol):
+    """
+    Parses a RAD line from the property block
+
+    M RADnn8 aaa vvv ...
+    aaa An atom number to alter the radical value of
+    vvv A key into the radical_dict to set the radical value to
+    """
+    if len(line) == 0:
+        return mol
     for k in mol:
         if k.startswith('?atom'):
-        #if k[0:4] == "atom":
             mol[k]["charge"] = "0"
             mol[k]["radical"] = "no radical"
 
-    for i in range(11,len(line),6):
-        aaa = line[i:i+3].strip()
-        vvv = int(float(line[i+3:i+6].strip()))
-        mol["?atom"+aaa]["radical"] = radical_dict[vvv]
+    line = line.rsplit(r'\s+')[3:]
+    for i in range(0,len(line),2):
+        aaa = int(line[i])
+        vvv = int(line[i+1])
+        mol["?atom%04d"%aaa]["radical"] = radical_dict[vvv]
+    return mol
 
 def apply_m_iso(line,mol):
+    """
+    Parses and applies an ISO line from the property block
+
+    M ISOnn8 aaa vvv ...
+    aaa An atom numer to alter the mass_dif of
+    vvv The mass_diff value to set
+    """
+    if len(line) == 0:
+        return mol
     for k in mol:
         if k.startswith('?atom'):
-        #if k[0:4] == "atom":
             mol[k]["mass_diff"] = "0"
 
-    for i in range(11,len(line),6):
-        aaa = line[i:i+3].strip()
-        vvv = line[i+3:i+6].strip()
-        mol["?atom"+aaa]["mass_diff"] = "+"+radical_dict[vvv]
+    line = line.rsplit(r'\s+')[3:]
+    for i in range(0,len(line),2):
+        aaa = int(line[i])
+        vvv = int(line[i]+1)
+        mol["?atom%04d"%aaa]["mass_diff"] = "+"+radical_dict[vvv]
+    return mol
 
+def merge_m_lines(lines):
+    if len(lines) == 0:
+        return ''
+    line0 = lines[0]
+    for l in lines[1:]:
+        line0 += l[8:]
+    return line0
 
-def parse_mol(lines, verbose = False, properties=False):
+def parse_mol(lines, verbose = False, data_items=False, tuple_realtions=False):
     """
-
     Parse the provided molfile and return a structured object representation
     that can be read by TRESTLE.
-    
     """
     mol = {}
     num_atoms = 0
@@ -201,6 +248,7 @@ def parse_mol(lines, verbose = False, properties=False):
     if lines[2]:
         mol["comment"] = lines[2].strip()
 
+    ### COUNTS LINE ###
     c_line = parse_counts_line(lines[3])
     num_atoms = c_line["aaa"]
     num_bonds = c_line["bbb"]
@@ -217,7 +265,7 @@ def parse_mol(lines, verbose = False, properties=False):
 
     mol["version"] = c_line["vvvvv"]
 
-
+    ### ATOM BLOCK ###
     for l,line in enumerate(lines[4:atom_dex]):
         atom = {}
         a_line = parse_atom_line(line)
@@ -242,64 +290,63 @@ def parse_mol(lines, verbose = False, properties=False):
             atom["hydrogen_count"] = h_count_dict[a_line["hhh"]]
 
 
-        mol["?atom"+str(l+1)] = atom
+        mol["?atom%04d"%(l+1)] = atom
 
+    ### BOND BLOCK ###
     for l,line in enumerate(lines[atom_dex:bond_dex]):
         bond = ['bond']
         b_line = parse_bond_line(line)
 
         bond.append(bond_type_dict[b_line["ttt"]])
         if bond_type_dict[b_line["ttt"]] == "Single":
-            bond.append(single_bond_stereo_dict[b_line["sss"]].replace(' ','_'))
+            bond.append(single_bond_stereo_dict[b_line["sss"]])
         elif bond_type_dict[b_line["ttt"]] == "Double":
-            bond.append(double_bond_stereo_dict[b_line["sss"]].replace(' ','_'))
-        bond.append("?atom"+str(b_line["111"]))
-        bond.append("?atom"+str(b_line["222"]))
+            bond.append(double_bond_stereo_dict[b_line["sss"]])
+        bond.append("?atom%04d"%b_line["111"])
+        bond.append("?atom%04d"%b_line["222"])
 
-        mol['('+' '.join(bond)+')'] = True
+        if tuple_realtions:
+            mol[tuple(bond)] = True
+        else:
+            mol['('+' '.join(bond)+')'] = True
 
+    ### PROPERTIES BLOCK ###
     Ms = []
     for line in lines[bond_dex:]:
+        if line == 'M  END':
+            break
         if line[0] == "M":
             Ms.append(line)
         else:
             break
     
-    if len(Ms) > 1:
-        if len(Ms) > 2:
-            for m in Ms[1:-1]:
-                Ms[0] += m[11:]
-        
-        if Ms[0][3:6] == "CHG":
-            apply_m_chg(Ms[0],mol)
+    if len(Ms) > 0:
+        apply_m_chg(merge_m_lines([l for l in Ms if l.startswith('M  CHG')]),mol)
+        apply_m_rad(merge_m_lines([l for l in Ms if l.startswith('M  RAD')]),mol)
+        apply_m_iso(merge_m_lines([l for l in Ms if l.startswith('M  ISO')]),mol)
 
-        elif Ms[0][3:6] == "RAD":
-            apply_m_rad(Ms[0],mol)
-
-        elif Ms[0][3:6] == "ISO":
-            apply_m_iso(Ms[0],mol)
-
-    if properties:
-        prop_dex = bond_dex + len(Ms)
+    ### DATA ITEMS ###
+    if data_items:
+        data_dex = bond_dex + len(Ms)
         opened = False
-        prop_header = ""
-        prop_list = []
-        for line in lines[prop_dex:]:
+        data_header = ""
+        data_list = []
+        for line in lines[data_dex:]:
             if line:
                 if not opened and line[0] == ">":
                     opened = True
-                    prop_header = parse_prop_name(line)
-                    prop_list = []
+                    data_header = parse_data_name(line)
+                    data_list = []
                     continue
                 elif opened:
-                    prop_list.append(line)
+                    data_list.append(line)
             elif opened:
-                mol[prop_header] = '\n'.join(prop_list)
+                mol[data_header] = '\n'.join(data_list)
                 opened = False
 
     return mol
 
-def parse_molefile(filename,verbose = False,properties=False,n=-1):
+def parse_sdf_file(filename,verbose = False,data_items=False,tuple_realtions=False,n=-1):
     ret = []
     curr = []
     count = 0
@@ -310,7 +357,7 @@ def parse_molefile(filename,verbose = False,properties=False,n=-1):
                 curr.append(line)
             else:
                 if len(curr) > 0:
-                    ret.append(parse_mol(curr,verbose,properties))    
+                    ret.append(parse_mol(curr,verbose,data_items,tuple_realtions))    
                     count += 1
                     if n > 0 and count > n:
                         break
@@ -318,6 +365,56 @@ def parse_molefile(filename,verbose = False,properties=False,n=-1):
     return ret
 
 if __name__ == '__main__':
-    mols = parse_molefile("./example.mol",True,True,n=5)
-    #mols = parse_molefile("./testSDFs/Compound_000000001_000025000.sdf",True,True,n=1)
-    pprint(mols)
+    mols = parse_sdf_file("./example.mol",True,True,n=5)
+    pprint(mols[0])
+    
+    # mols = parse_sdf_file("./testSDFs/Compound_000000001_000025000.sdf",True,True)
+    # bond_counts = ContinuousValue()
+    # atom_counts = ContinuousValue()
+
+    # bmax = -10000000
+    # bmin = 100000000
+    # amax = -10000000
+    # amin = 100000000
+
+    # a_dist = {}
+    # b_dist = {}
+
+    # for mol in mols:
+    #     atoms = [v for v in mol if isinstance(v,str) and v.startswith('?atom')]
+    #     bons = [v for v in mol if isinstance(v,str) and v.startswith('(bond')]
+
+    #     b_count = len(bons)
+    #     a_count = len(atoms)
+
+    #     if a_count < amin:
+    #         amin = a_count
+    #     if a_count > amax:
+    #         amax = a_count
+
+    #     if b_count < bmin:
+    #         bmin = b_count
+    #     if b_count > bmax:
+    #         bmax = b_count
+
+    #     bond_counts.update(b_count)
+    #     atom_counts.update(a_count)
+
+    #     if a_count not in a_dist:
+    #         a_dist[a_count] = 0
+    #     if b_count not in b_dist:
+    #         b_dist[b_count] = 0
+
+    #     a_dist[a_count] += 1
+    #     b_dist[b_count] += 1
+
+    # print('Bonds',bond_counts)
+    # print('Min:',bmin,'Max:',bmax)
+    # print('Atoms',atom_counts)
+    # print('Min:',amin,'Max:',amax)
+
+    # with open('atom_dist.txt','w') as out:
+    #     out.write('AtomCount\tFrequency\n')
+    #     keys = list(a_dist.keys())
+    #     for k in sorted(keys):
+    #         out.write(str(k)+'\t'+str(a_dist[k])+'\n')
